dd47735334c6af6d0b6a80c76770f7b4a518ae2b
vadymovychvadym@gmail.com <vadymovychvadym@gmail.com>
2019-04-15 23:10:39 +0300

    Initial Commit

---
+++ MyRegViewer/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace MyRegViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 12);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(346, 413);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(364, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(424, 413);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Registry Viewer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ MyRegViewer/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace MyRegViewer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+
+            RegistryKey[] regs = new RegistryKey[] { Registry.ClassesRoot, Registry.CurrentUser, //};
+                Registry.LocalMachine, Registry.Users, Registry.CurrentConfig};
+            listView1.Columns.Add("Имя", 100);
+            listView1.Columns.Add("Тип", 100);
+            listView1.Columns.Add("Значение", 150);
+            listView1.View = View.Details;
+            toolStripStatusLabel1.Text = regs[0].Name;
+            foreach (RegistryKey key in regs)
+            {
+                TreeNode node = new TreeNode(key.Name);
+                node.Tag = key;
+                await Task.Run(() => AddNodes(node, key, 0));
+                treeView1.Nodes.Add(node);
+            }
+        }
+
+        private void AddNodes(TreeNode parentNode, RegistryKey key, int depth)
+        {
+            if (depth < 2)
+            {
+                depth++;
+                string[] subKeys = key.GetSubKeyNames();
+                if (subKeys.Length > 0)
+                {
+                    for (int i = 0; i < subKeys.Length; i++)
+                    {
+                        try
+                        {
+                            TreeNode node = new TreeNode(subKeys[i]);
+                            UpdateTreeView(parentNode, node);
+                            RegistryKey subKey = key.OpenSubKey(subKeys[i]);
+                            node.Tag = subKey;
+                            AddNodes(node, subKey, depth);
+                        }
+                        catch { }
+                    }
+                }
+            }
+            
+        }
+
+        private void UpdateTreeView(TreeNode parentNode, TreeNode childNode)
+        {
+            if (treeView1.InvokeRequired)
+            {
+                treeView1.Invoke(new Action<TreeNode, TreeNode>(UpdateTreeView), parentNode, childNode);
+            }
+            else
+            {
+                parentNode.Nodes.Add(childNode);
+            }
+        }
+
+        private async void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            listView1.Items.Clear();
+            toolStripStatusLabel1.Text = e.Node.FullPath;
+            TreeNode node = e.Node;
+            if (node.Tag is RegistryKey)
+            {
+                RegistryKey selectedKey = node.Tag as RegistryKey;
+                string[] names = selectedKey.GetValueNames();
+                foreach (string str in names)
+                {
+                    try
+                    {
+                        ListViewItem item = null;
+                        if (string.IsNullOrEmpty(str))
+                            item = new ListViewItem("(По умолчанию)");
+                        else
+                            item = new ListViewItem(str);
+                        RegistryValueKind valueKind = selectedKey.GetValueKind(str);
+                        item.SubItems.Add(valueKind.ToString());
+                        object keyValue = selectedKey.GetValue(str);
+                        string stringValue = "";
+                        if (keyValue == null || string.IsNullOrEmpty(keyValue.ToString()))
+                        {
+                            stringValue = "(значение не присвоено)";
+                        }
+                        else
+                        if (valueKind == RegistryValueKind.Binary)
+                        {
+                            byte[] buff = (byte[])keyValue;
+                            string temp = "";
+                            foreach (byte elem in buff)
+                            {
+                                temp += $"{elem:X4}";
+                            }
+                            stringValue = temp;
+                        }
+                        else
+                        {
+                            stringValue = keyValue.ToString();
+                        }
+                        item.SubItems.Add(stringValue);
+                        listView1.Items.Add(item);
+                    }
+                    catch { }
+                }
+                
+                if(e.Node.FullPath.Contains(@"\"))
+                {
+                    node.Nodes.Clear();
+                    await Task.Run(() => AddNodes(node, selectedKey, 0));
+                }
+            }
+        }
+    }
+}
